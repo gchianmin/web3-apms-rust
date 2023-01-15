f763265d25a62741ce9d0aa181a6906deca5cc9a
gchianmin <min3568@hotmail.com>
2023-01-15 21:41:08 +0800

    shifted frontend files to another folder

--- src/apmsdapp/programs/apmsdapp/src/lib.rs
+++ src/apmsdapp/programs/apmsdapp/src/lib.rs
@@ -1,15 +1,96 @@
 use anchor_lang::prelude::*;
-
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// use chrono::DateTime;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+declare_id!("51QHpF5dYpwRfRfLAiBShGHEKZ3hT5K1FTyoo9omKfoz");
 
 #[program]
 pub mod apmsdapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    // create a conference
+    pub fn create(ctx: Context<Create>, name: String, description: String, date: String, venue: String, submission_deadline: String ) -> ProgramResult {
+        let conference = &mut ctx.accounts.conference;
+        conference.name = name;
+        conference.description = description;
+        conference.date = date;
+        conference.venue = venue;
+        conference.submission_deadline = submission_deadline;
+        conference.paper_submitted = 0; //put as 0 for now, to change to paper submitted object
+        conference.fee_received = 0;
+        conference.admin = *ctx.accounts.user.key;
         Ok(())
     }
+
+    // modify the details of a conference
+    pub fn modify(ctx: Context<Modify>, new_name: String, new_description: String, new_date: String, new_venue: String, new_submission_deadline: String) -> ProgramResult {
+        ctx.accounts.conference.name = new_name;
+        ctx.accounts.conference.description = new_description;
+        ctx.accounts.conference.date = new_date;
+        ctx.accounts.conference.venue = new_venue;
+        ctx.accounts.conference.submission_deadline = new_submission_deadline;
+        Ok(())
+    }
+
+    // cancel a conference
+    pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
+        let conference = &mut ctx.accounts.conference;
+        let user = &mut ctx.accounts.user;
+        //check if the user calling this function is the admin 
+        if conference.admin != *user.key {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        let lamport: u64 = 9333360 - 890880;
+        **conference.to_account_info().try_borrow_mut_lamports()? -= lamport;
+        **user.to_account_info().try_borrow_mut_lamports()? += lamport;
+        Ok(())
+    }
+
+}
+
+//macro - used to indicate that this is a context
+#[derive(Accounts)]
+pub struct Create<'info> {
+    //use init here but not elsewhere because we are creating a new conference account
+    #[account[init, payer=user, space=9000, seeds=[b"CONFERENCE".as_ref(), user.key().as_ref()], bump]]
+    pub conference: Account<'info, Conference>,
+    
+    //specify that the user account is mutable - we can change it
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+
+    // conference account needs to be a program-derived account - to send money to admin - cannot be an account controlled by any users.
+    // hence adding seeds in the conference account macro; bump is needed for solana to find an account address that is not being used yet
+    //creating account but also need to specify how the account shud look like hence the struct below
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Modify<'info> {
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"CONFERENCE", user.key().as_ref()], bump)]
+    pub conference: Account<'info, Conference>,
+}
+
+#[derive(Accounts)]
+pub struct Cancel<'info> {
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"CONFERENCE", user.key().as_ref()], bump, close = user)]
+    pub conference: Account<'info, Conference>,
+    
+    // /// CHECK: This is not dangerous
+    // #[account(mut, signer)]
+    // pub user: AccountInfo<'info>,
+    // pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Conference {
+    pub admin: Pubkey, //able to withdraw fee paid by the authors
+    pub name: String,
+    pub description: String,
+    pub date: String,
+    pub venue: String,
+    pub submission_deadline: String,
+    pub paper_submitted: u64,
+    pub fee_received: u64
+}
--- src/apmsdapp/tests/apmsdapp.ts
+++ src/apmsdapp/tests/apmsdapp.ts
@@ -1,16 +1,100 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Apmsdapp } from "../target/types/apmsdapp";
+const assert = require('assert')
+const anchor = require('@project-serum/anchor')
+const { SystemProgram } = anchor.web3
+const { PublicKey } = require('@solana/web3.js')
 
 describe("apmsdapp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // const provider = anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
+  anchor.setProvider(provider);
+  // const conference = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Apmsdapp;
 
-  const program = anchor.workspace.Apmsdapp as Program<Apmsdapp>;
+  //it block represents one test
+  it('Creates a conference', async () => {
+    const [conferencePDA, _] = await PublicKey
+      .findProgramAddressSync(
+        [
+          anchor.utils.bytes.utf8.encode("CONFERENCE"),
+          provider.wallet.publicKey.toBuffer()
+        ],
+        program.programId
+      );
+    //call create fn and pass it init message
+    await program.rpc.create("IEEE Conference", "A yearly conference for authors globally.", "2023-04-05 00:00:00", "Marina Bay Sand", "2023-02-05 00:00:00", {
+      accounts: {
+        conference: conferencePDA,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      //need pass in as we are creating a new cal acc
+      // signers: [conferencePDA]
+    })
+    const account = await program.account.conference.fetch(conferencePDA)
+    console.log(conferencePDA)
+    assert.ok(account.name === "IEEE Conference")
+    assert.ok(account.description === "A yearly conference for authors globally.")
+    assert.ok(account.date === "2023-04-05 00:00:00")
+    assert.ok(account.venue === "Marina Bay Sand")
+    assert.ok(account.submissionDeadline === "2023-02-05 00:00:00")
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  })
+
+  it('Modifying a conference', async () => {
+    const [conferencePDA, _] = await PublicKey
+      .findProgramAddressSync(
+        [
+          anchor.utils.bytes.utf8.encode("CONFERENCE"),
+          provider.wallet.publicKey.toBuffer()
+        ],
+        program.programId
+      );
+
+    await program.rpc.modify("ACM Conference", "A yearly conference for authors globally.", "2023-05-05 00:00:00", "Suntec Convention Centre", "2023-03-05 00:00:00", {
+      accounts: {
+        conference: conferencePDA,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+
+    })
+    const account = await program.account.conference.fetch(conferencePDA)
+    console.log(conferencePDA)
+    assert.ok(account.name === "ACM Conference")
+    assert.ok(account.description === "A yearly conference for authors globally.")
+    assert.ok(account.date === "2023-05-05 00:00:00")
+    assert.ok(account.venue === "Suntec Convention Centre")
+    assert.ok(account.submissionDeadline === "2023-03-05 00:00:00")
+  })
+
+  it('Canceling a conference', async () => {
+
+    const [conferencePDA, _] = await PublicKey
+      .findProgramAddressSync(
+        [
+          anchor.utils.bytes.utf8.encode("CONFERENCE"),
+          provider.wallet.publicKey.toBuffer()
+        ],
+        program.programId
+      );
+
+    await program.rpc.cancel({
+      accounts: {
+        conference: conferencePDA,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+    })
+
+    await assert.rejects(
+      async () => {
+        const acc = await program.account.conference.fetch(conferencePDA)
+      },
+      {
+        name: 'Error',
+        message: `Account does not exist ${conferencePDA.toString()}`,
+      },
+    );
+  })
 });
