8e6df1d132eed717f99d4dd3e1c2978789d3ab3f
gchianmin <73581303+gchianmin@users.noreply.github.com>
2023-06-23 00:54:37 +0800

    Test/unit test (#9)
    * corrected unit tests
    
    * added vercel web analytics

--- src/apmsdapp-frontend/pages/_app.js
+++ src/apmsdapp-frontend/pages/_app.js
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Layout from "../components/Layout";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </UserProvider>
   );
